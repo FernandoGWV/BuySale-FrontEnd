becfd8ad3ab18a8a3b3f8bd39a3fb92a683662bb
Fernando Guedes <luizwinther09@gmail.com>
2024-02-09 19:59:54 -0300

    Form Register Product

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -3,29 +3,25 @@ import Button from "@/components/Button";
 import { UserContextAuth } from "@/context/userContext";
 import Api from "@/services/Api";
 import { useEffect, useState } from "react";
-
+import { useForm } from "react-hook-form";
 const Register = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    wallet: "",
-    iconUser: null,
-  });
-  let dataRegister;
-  const handleOnChange = (e: any) => {
-    const { name, value, files } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: files ? files[0] : value,
-    }));
-  };
-  const handleSubmit = async (e: any) => {
+  const { register, handleSubmit } = useForm();
+  const { dataUser } = UserContextAuth();
+  const onSubmit = async (data: any) => {
+    const { files } = data;
+    console.log(data);
     try {
-      const result = await Api.post("account/createAccount", formData, {
+      const resultProduct = await Api.post(
+        `/products/create/${1}`,
+        { ...data, files: null },
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      /*   const result = await Api.post("/imagesUpload", files, {
         headers: { "Content-Type": "multipart/form-data" },
       });
-      return (dataRegister = result.data);
+      console.log(result.data); */
     } catch (error) {
       console.log(error);
     }
@@ -36,58 +32,38 @@ const Register = () => {
       <section className="container mx-auto flex items-center h-screen justify-center ">
         <div className="shadow-xl w-96 h-96 flex flex-col justify-center items-center">
           <h1 className="uppercase text-xl w-fit mb-4">cadastro</h1>
-          <form
-            method="post"
-            className="flex flex-col gap-4  w-fit "
-            encType="multipart/form-data"
-            onSubmit={handleSubmit}
-          >
+          <div className="flex flex-col gap-4  w-fit">
             <input
               type="text"
-              placeholder="name"
-              name="name"
+              placeholder="Titulo do Produto..."
+              {...register("title")}
               className="w-80 p-2 rounded outline-none shadow-md border 
               border-gray-50"
-              value={formData.name}
-              onChange={handleOnChange}
-            />
-            <input
-              type="email"
-              placeholder="email"
-              name="email"
-              className="w-80 p-2 rounded outline-none shadow-md border 
-              border-gray-50"
-              value={formData.email}
-              onChange={handleOnChange}
-            />
-            <input
-              type="password"
-              placeholder="senha"
-              name="password"
-              className="w-80 p-2 rounded outline-none shadow-md border 
-              border-gray-50"
-              value={formData.password}
-              onChange={handleOnChange}
             />
             <input
               type="text"
-              placeholder="Valor a ser adicionado a carteira..."
-              name="wallet"
+              placeholder="Descrição do Produto..."
+              {...register("descripte")}
               className="w-80 p-2 rounded outline-none shadow-md border 
               border-gray-50"
-              value={formData.wallet}
-              onChange={handleOnChange}
             />
             <input
-              type="file"
-              name="iconUser"
-              id="iconUser"
-              onChange={handleOnChange}
+              type="number"
+              placeholder="Valor do produto"
+              {...register("price")}
+              className="w-80 p-2 rounded outline-none shadow-md border 
+              border-gray-50"
             />
+            <input type="file" multiple {...register("files")} id="files" />
             <span></span>
 
-            <Button text={"Cadastrar-se"} />
-          </form>
+            <Button
+              text={"Cadastrar-se"}
+              handleFunction={() => {
+                handleSubmit(onSubmit)();
+              }}
+            />
+          </div>
         </div>
       </section>
     </>
