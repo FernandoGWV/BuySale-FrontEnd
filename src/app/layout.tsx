4ff6728594445526a634ba9cf7791cd160c196cc
Fernando Guedes <luizwinther09@gmail.com>
2024-03-27 08:32:13 -0300

    Upload de image failed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import UserAuthContext from "@/context/userContext";
 import Header from "./components/header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata: Metadata = {
   title: "Buy&Sale",
@@ -16,7 +18,11 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <UserAuthContext>
-        <body> {children}</body>
+        <body>
+          {" "}
+          {children}
+          <ToastContainer />
+        </body>
       </UserAuthContext>
     </html>
   );
--- src/app/products/register/page.tsx
+++ src/app/products/register/page.tsx
@@ -3,31 +3,66 @@ import Button from "@/app/components/Button";
 import { UserContextAuth } from "@/context/userContext";
 import Api from "@/services/Api";
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { GoArrowLeft } from "react-icons/go";
+import { toast } from "react-toastify";
+
 const Register = () => {
   const router = useRouter();
   const { dataUser } = UserContextAuth();
-  const { register, handleSubmit, formState: {errors} } = useForm();
-  let formDataValue;
-  const handleImage: any = useRef<HTMLInputElement>(null)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleImage: any = useRef<HTMLInputElement>(null);
+  const [imagesPreview, setImagePreview] = useState<[]>([]);
+
+  const handleFileChange = (event: any) => {
+    const files = event.target.files;
+    const imagesPreviewArray: any = [];
+
+    if (files && files.length <= 4) {
+      for (let i = 0; i < files.length; i++) {
+        const reader: any = new FileReader();
+        const file = files[i];
+
+        reader.onload = () => {
+          imagesPreviewArray.push(reader.result);
+          // Verifica se todas as imagens foram processadas
+          if (imagesPreviewArray.length === files.length) {
+            setImagePreview(imagesPreviewArray);
+          }
+        };
+
+        reader.readAsDataURL(file);
+      }
+    } else {
+      // Se mais de 4 arquivos forem selecionados, faça o tratamento adequado aqui
+      toast.error("Só pode enviar 4 imagens no máximo!");
+      // Limpa a seleção atual
+      event.target.value = null;
+    }
+  };
+
   const onSubmit = async (data: any) => {
-    formDataValue = data;
     const { files, title, descripte, price } = data;
+
     try {
       const resultProduct = await Api.post(`/products/create/${dataUser?.id}`, {
         title,
         descripte,
         price,
       }).then(async (dados: any) => {
+        console.log(dados);
+        toast.success(`${dados.data.message}`);
         if (dados.data.status) {
           const result = await Api.post(
             `/imagesUpload/${dados.data.product.id}`,
             files
           );
           router.push("/products");
-          return result;
         }
       });
     } catch (error) {
@@ -43,67 +78,105 @@ const Register = () => {
             Cadastrar novo produto
           </h1>
           <div className="flex flex-col gap-4  w-fit">
-
             <div className="relative">
+              <input
+                type="text"
+                placeholder="Titulo do Produto..."
+                {...register("title", { required: true })}
+                className="w-80 p-2 rounded outline-none shadow-md border 
+              border-gray-50"
+              />
+              <span className="text-red-600 text-2xl absolute top-3 ml-3 ">
+                *
+              </span>
+              {errors.title && (
+                <span className="text-red-600">
+                  Campo titulo obrigatório ser preenchido
+                </span>
+              )}
+            </div>
+            <div className="relative">
+              <textarea
+                className="w-80 overflow-auto p-2 rounded outline-none shadow-md border 
+              border-gray-50"
+                placeholder="Descrição do Produto..."
+                cols={30}
+                {...register("descripte", { required: true })}
+              ></textarea>
+              <span className="text-red-600 text-2xl absolute top-3 ml-3 ">
+                *
+              </span>
+              {errors.descripte && (
+                <span className="text-red-600">
+                  Campo descrição obrigatório ser preenchido
+                </span>
+              )}
+            </div>
+            <div className="relative">
+              <input
+                type="number"
+                placeholder="Valor do produto"
+                {...register("price", { required: true })}
+                className="w-80 p-2 rounded outline-none shadow-md border 
+              border-gray-50"
+              />
+              <span className="text-red-600 text-2xl absolute top-3 ml-3 ">
+                *
+              </span>
+              {errors.price && (
+                <span className="text-red-600">
+                  Campo preço obrigatório ter um valor
+                </span>
+              )}
+            </div>
             <input
-              type="text"
-              placeholder="Titulo do Produto..."
-              {...register("title", {required: true})}
-              className="w-80 p-2 rounded outline-none shadow-md border 
-              border-gray-50"
+              ref={handleImage}
+              hidden
+              onChange={handleFileChange}
+              type="file"
+              multiple
+              id="files"
             />
-           <span className="text-red-600 text-2xl absolute top-3 ml-3 ">*</span>
-            {errors.title && <span className="text-red-600">Campo titulo obrigatório ser preenchido</span>}
-            </div>
-
             <div className="relative">
-            <textarea
-              className="w-80 overflow-auto p-2 rounded outline-none shadow-md border 
-              border-gray-50"
-              placeholder="Descrição do Produto..."
-              {...register("descripte", {required: true})}
-              name=""
-              id=""
-              cols={30}
-            ></textarea>
-        <span className="text-red-600 text-2xl absolute top-3 ml-3 ">*</span>
-            {errors.descripte && <span className="text-red-600">Campo descrição obrigatório ser preenchido</span>}
+              <button
+                className="bg-white text-myColor border-2 border-myColor rounded p-2 w-fit"
+                onClick={() => {
+                  handleImage.current.click();
+                }}
+              >
+                ESCOLHER IMAGENS
+              </button>
+              <span className="text-red-600 text-2xl absolute top-3 ml-3 ">
+                *
+              </span>
+              <span className="block">
+                Só pode enviar no máximo 4 imagens por produto
+              </span>
             </div>
-
-            <div className="relative">
-            <input
-              type="number"
-              placeholder="Valor do produto"
-              {...register("price",{required: true})}
-              className="w-80 p-2 rounded outline-none shadow-md border 
-              border-gray-50"
-            />
-             <span className="text-red-600 text-2xl absolute top-3 ml-3 ">*</span>
-            {errors.price && <span className="text-red-600">Campo preço obrigatório ter um valor</span>}
+            <div className="flex items-center   gap-5">
+              {imagesPreview &&
+                imagesPreview.map((item: any) => {
+                  return (
+                    <>
+                      <div
+                        className="w-32 h-32"
+                        style={{
+                          backgroundImage: `url(${item})`,
+                          backgroundRepeat: "no-repeat",
+                          backgroundSize: "cover",
+                          backgroundPosition: "center",
+                        }}
+                      ></div>
+                    </>
+                  );
+                })}
             </div>
-            
-            <input ref={handleImage} hidden type="file" multiple  id="files" />
-          
-            <div className="relative">
-            <button
-              className="bg-white text-myColor border-2 border-myColor rounded p-2 w-fit"
-              onClick={() => {
-               
-                handleImage.current.click();
-              }}
-            >
-              ESCOLHER IMAGENS
-            </button>
-            <span className="text-red-600 text-2xl absolute top-3 ml-3 ">*</span>
-            <span className="block">Só pode enviar no máximo 4 imagens por produto</span>
-            </div>
-
-           { formDataValue ? <Button
+            <Button
               text={"Cadastrar-se"}
               handleFunction={() => {
                 handleSubmit(onSubmit)();
               }}
-            /> : <button disabled={true} className={`text-red-50 max-w-40 p-1 rounded bg-myColor opacity-45 uppercase`}>Cadastrar-se</button>}
+            />
           </div>
           <button
             className="bg-myColor p-1 max-w-40 text-white rounded-md text-center uppercase px-2 mt-5"
